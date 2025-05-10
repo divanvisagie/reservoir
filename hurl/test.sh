973ed8ad59ad05c96f1f6fe8f954db9dca0d53be
Divan Visagie <me@divanv.com>
2025-05-10 11:23:53 +0200

    chore: add Hurl test scripts and endpoints for chat completion and reservoir view
    - Add hurl/chat_completion.hurl for chat completion endpoint testing
    - Add hurl/reservoir-view.hurl for reservoir view endpoint testing
    - Add hurl/test.sh script with blue bold output for running tests

---
+++ hurl/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+echo_blue_bold() {
+  echo -e "\033[1;34m$1\033[0m"
+}
+
+echo_blue_bold " hurl/chat_completion.hurl >"
+hurl --variable USER="$USER" --variable OPENAI_API_KEY="$OPENAI_API_KEY" hurl/chat_completion.hurl
+echo ""
+echo ""
+echo_blue_bold " hurl/reservoir-view.hurl >"
+hurl --variable USER="$USER" --variable OPENAI_API_KEY="$OPENAI_API_KEY" hurl/reservoir-view.hurl
