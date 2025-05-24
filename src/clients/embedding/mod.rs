e8dd4d5ad314ccc0c3720f2362e05d407fd8ddd2
Divan Visagie <me@divanv.com>
2025-05-24 02:29:19 +0200

    started working on local models for embedding

---
+++ src/clients/embedding/mod.rs
@@ -0,0 +1,68 @@
+use std::path::PathBuf;
+
+use super::openai::embeddings::get_embeddings_for_text as openai_get_embeddings_for_text;
+use anyhow::Error;
+use fastembed::{EmbeddingModel, InitOptions, TextEmbedding};
+use tracing::info;
+
+pub enum EmbeddingClient {
+    OpenAI(String),
+    FastEmbed(String),
+}
+
+impl EmbeddingClient {
+    pub fn new_openai(model: String) -> Self {
+        EmbeddingClient::OpenAI(model)
+    }
+
+    pub fn new_fastembed(model: &str) -> Self {
+        EmbeddingClient::FastEmbed(model.to_string())
+    }
+
+    pub fn default() -> Self {
+        EmbeddingClient::OpenAI("text-embedding-ada-002".to_string())
+    }
+}
+
+pub fn get_cache_path() -> PathBuf {
+    let tmp_dir = dirs_next::data_dir().unwrap();
+    tmp_dir.join("reservoir").join("models")
+}
+
+pub async fn get_embeddings_for_txt(
+    text: &str,
+    client: EmbeddingClient,
+) -> Result<Vec<f32>, Error> {
+    match client {
+        EmbeddingClient::OpenAI(model) => {
+            let result = openai_get_embeddings_for_text(text).await;
+            match result {
+                Ok(embeddings) => {
+                    if embeddings.is_empty() {
+                        Err(Error::msg("No embeddings found"))
+                    } else {
+                        Ok(embeddings[0].embedding.clone())
+                    }
+                }
+                Err(e) => Err(e),
+            }
+        }
+        EmbeddingClient::FastEmbed(_model) => {
+            info!("Using FastEmbed for embedding");
+            let init_options = InitOptions::new(EmbeddingModel::BGELargeENV15)
+                .with_show_download_progress(true)
+                .with_cache_dir(get_cache_path());
+
+            let model = TextEmbedding::try_new(init_options);
+            let texts = vec![text];
+            let model = model?;
+            let embeddings = model.embed(texts, None)?;
+
+            if let Some(embedding) = embeddings.first() {
+                Ok(embedding.clone())
+            } else {
+                Err(Error::msg("No embeddings found"))
+            }
+        }
+    }
+}
--- src/commands/ingest.rs
+++ src/commands/ingest.rs
@@ -1,11 +1,12 @@
-use crate::repos::message::{AnyMessageRepository, MessageRepository};
-use crate::models::message_node::MessageNode;
-use crate::clients::openai::embeddings::get_embeddings_for_text;
-use crate::clients::openai::types::Message;
-use anyhow::Error;
-use uuid::Uuid;
-use std::io::{self, Read};
 use crate::args::IngestSubCommand;
+use crate::clients::embedding::{get_embeddings_for_txt, EmbeddingClient};
+use crate::clients::openai::types::Message;
+use crate::models::message_node::MessageNode;
+use crate::repos::message::{AnyMessageRepository, MessageRepository};
+use anyhow::Error;
+use std::io::{self, Read};
+use tracing::info;
+use uuid::Uuid;
 
 pub async fn run(repo: &AnyMessageRepository, cmd: &IngestSubCommand) -> Result<(), Error> {
     // Read stdin
@@ -16,7 +17,10 @@ pub async fn run(repo: &AnyMessageRepository, cmd: &IngestSubCommand) -> Result<
         println!("No input provided on stdin");
         return Ok(());
     }
-    let partition = cmd.partition.clone().unwrap_or_else(|| "default".to_string());
+    let partition = cmd
+        .partition
+        .clone()
+        .unwrap_or_else(|| "default".to_string());
     let instance = cmd.instance.clone().unwrap_or_else(|| partition.clone());
     let trace_id = Uuid::new_v4().to_string();
     let role = cmd.role.clone().unwrap_or_else(|| "user".to_string());
@@ -29,9 +33,15 @@ pub async fn run(repo: &AnyMessageRepository, cmd: &IngestSubCommand) -> Result<
         role,
         content: content.clone(),
     };
-    let embedding = get_embeddings_for_text(&content).await?.first().unwrap().embedding.clone();
+    let client = EmbeddingClient::default();
+    let test_local = EmbeddingClient::new_fastembed("");
+    let embedding = get_embeddings_for_txt(&content, client).await?;
+    let embedding_test = get_embeddings_for_txt(&content, test_local).await?;
+
+    info!("Embedding test: {:?}", embedding_test);
+
     let node = MessageNode::from_message(&message, &trace_id, &partition, &instance, embedding);
     repo.save_message_node(&node).await?;
     println!("Saved message with trace_id: {}", trace_id);
     Ok(())
-} 
+}
